a0f96953452244be188db7ffb8a722611952ce65
Le119 <you@example.com>
2024-03-20 12:04:44 -0400

    divided T20 and SPWL policies into separate mortality tables

--- mortality_rate.py
+++ mortality_rate.py
@@ -64,6 +64,33 @@ def add_lapse_age(data):
             row.append("NA")
     return data
 
+def divide_T20_SPWL(data):
+    T20 = []
+    SPWL = []
+    for row in data:
+        if row[1] == "T20":
+            T20 += [row]
+        else:
+            SPWL += [row]
+    return (T20, SPWL)
+
+def make_mortality_table(data):
+    c = 0
+    print("min age " + str(find_min_issue_age(data)))
+    print("max age " + str(find_max_death_age(data)))
+    mortality_table = []
+    for u in range(find_min_issue_age(data), find_max_death_age(data) + 1):
+        dead_u = death_age(u, data)
+        issue_u = issue_age(u, data)
+        lapse_u = lapse_age(u, data)
+        (mu, c) = motality_rate(len(issue_u), len(dead_u), len(lapse_u), c)
+        print("age " + str(u))
+        print(len(issue_u), len(dead_u), len(lapse_u))
+        print(mu, c)
+        mortality_table.append([u, mu])
+    print(mortality_table)
+    return mortality_table
+
 if __name__ == '__main__':
 
     with open('2024-srcsc-superlife-inforce-dataset.csv', mode='r') as csv_file:
@@ -73,28 +100,23 @@ if __name__ == '__main__':
         data = data[4:]
         data = add_death_age(data)
         data = add_lapse_age(data)
+        T20, SPWL = divide_T20_SPWL(data)
         
-        c = 0
-        print("min age " + str(find_min_issue_age(data)))
-        print("max age " + str(find_max_death_age(data)))
-        mortality_table = []
-        for u in range(find_min_issue_age(data), find_max_death_age(data) + 1):
-            dead_u = death_age(u, data)
-            issue_u = issue_age(u, data)
-            lapse_u = lapse_age(u, data)
-            (mu, c) = motality_rate(len(issue_u), len(dead_u), len(lapse_u), c)
-            print("age " + str(u))
-            print(len(issue_u), len(dead_u), len(lapse_u))
-            print(mu, c)
-            mortality_table.append([u, mu])
-        print(mortality_table)
+        T20_mortality_table = make_mortality_table(T20)
+        SPWL_mortality_table = make_mortality_table(SPWL)
 
-
-    with open('inforce_motality_rate.csv', mode='w', newline='') as csv_file:
+    with open('T20_inforce_motality_rate.csv', mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Age", "Mortality Rate"])
 
-        for row in mortality_table:
+        for row in T20_mortality_table:
+            writer.writerow(row)
+    
+    with open('SPWL_inforce_motality_rate.csv', mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["Age", "Mortality Rate"])
+
+        for row in SPWL_mortality_table:
             writer.writerow(row)
         
 
